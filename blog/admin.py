090547013d1e597e52d8013ce80be91580cbdf01
Ислам <islam@MacBook-Pro-Islam.local>
2026-01-13 20:35:52 +0300

    blog views

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'status', 'publish']
+    list_filter = ['author', 'status', 'publish']
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
